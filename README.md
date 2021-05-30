1a5acd61f11df9aa92fdae6209f1c02e1f8a38b1
theolice <4408879+theolice@users.noreply.github.com>
2021-05-30 23:26:23 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+﻿#vieb-chocoreep
+
+This is the [Chocolatey package](https://community.chocolatey.org/packages/vieb/) repository for [Vieb](https://vieb.dev/) - the Vim Inspired Electron Browser.
+
+("chocoreep" is Dutch for chocolate bar, punintended)
---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,24 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://github.com/Jelmerro/Vieb/releases/download/5.0.0/Vieb.Setup.5.0.0.exe'
+$url64      = 'https://github.com/Jelmerro/Vieb/releases/download/5.0.0/Vieb.Setup.5.0.0.exe'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe'
+  url           = $url
+  url64bit      = $url64
+
+  softwareName  = 'vieb*'
+
+  checksum      = '7c16d63ef53560a232a1c6176f9cdd944de85d88060db0914c83150eb5555824'
+  checksumType  = 'sha256'
+  checksum64    = '7c16d63ef53560a232a1c6176f9cdd944de85d88060db0914c83150eb5555824'
+  checksumType64= 'sha256'
+
+  silentArgs    = "/S"
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
