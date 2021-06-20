5ad4870eb8530c0b03e4c650ee53c62f655968f5
theolice <4408879+theolice@users.noreply.github.com>
2021-06-20 22:51:12 +0200

    Version 5.2.0

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$ErrorActionPreference = 'Stop'; # stop on all errors
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://github.com/Jelmerro/Vieb/releases/download/5.1.0/Vieb.Setup.5.1.0.exe'
-$url64      = 'https://github.com/Jelmerro/Vieb/releases/download/5.1.0/Vieb.Setup.5.1.0.exe'
+$url        = 'https://github.com/Jelmerro/Vieb/releases/download/5.2.0/Vieb.Setup.5.2.0.exe'
+$url64      = 'https://github.com/Jelmerro/Vieb/releases/download/5.2.0/Vieb.Setup.5.2.0.exe'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -12,9 +12,9 @@ $packageArgs = @{
 
   softwareName  = 'vieb*'
 
-  checksum      = 'DED2753521EE65BC05CBE30385AEB795009AF1B4C8B54364DD2DCFC1F7E17DE5'
+  checksum      = '6EC33AD36F5E0220C9541E3BEEE87ED3F89DB653B6983DED8B7E1E2A911CC4F0'
   checksumType  = 'sha256'
-  checksum64    = 'DED2753521EE65BC05CBE30385AEB795009AF1B4C8B54364DD2DCFC1F7E17DE5'
+  checksum64    = '6EC33AD36F5E0220C9541E3BEEE87ED3F89DB653B6983DED8B7E1E2A911CC4F0'
   checksumType64= 'sha256'
 
   silentArgs    = "/S"
