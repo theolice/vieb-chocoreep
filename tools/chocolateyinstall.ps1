84051cf5c49c0a26fdf4935dc9767148d39475eb
theolice <4408879+theolice@users.noreply.github.com>
2022-07-03 15:18:07 +0200

    Update to v8.1.0

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop'; # stop on all errors
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://github.com/Jelmerro/Vieb/releases/download/7.2.0/Vieb.Setup.7.2.0.exe'
+$url        = 'https://github.com/Jelmerro/Vieb/releases/download/8.1.0/Vieb.Setup.8.1.0.exe'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -10,7 +10,7 @@ $packageArgs = @{
 
   softwareName  = 'vieb*'
 
-  checksum      = '0567ED41E1B1519118642CD37A5A9656B8C4D1CC15734C6B9E63DC5DA455B3A4'
+  checksum      = 'F4CEBE0FEDF8B25DAC4DB1A6B69A7452F2ED5B22DAE43C72CA7C3EDAB9882212'
   checksumType  = 'sha256'
 
   silentArgs    = "/S"
